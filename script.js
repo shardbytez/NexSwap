45788e72221b7506fbb2dfd63adab9bd8e2ab276
Coinbox17 <coinbox17@gmail.com>
2025-10-17 22:42:17 +0700

    Initial Win95-style front for NexSwap

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('connectWallet').addEventListener('click', () => {
+    alert('Wallet connected!');
+});
+
+document.getElementById('swap').addEventListener('click', () => {
+    alert('Swap clicked!');
+});
+
+document.getElementById('addLiquidity').addEventListener('click', () => {
+    alert('Add Liquidity clicked!');
+});
